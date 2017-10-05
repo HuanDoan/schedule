1199cb45c929480e77f330e87ce42631036cdcab
hieutran <tranhieu130196@gmail.com>
2017-10-05 10:43:19 +0700

    hoàn thành giao diện, viết js cho chữ chạy ngang dưới cùng

--- application/views/layout_web/homepage_view.php
+++ application/views/layout_web/homepage_view.php
@@ -1,46 +1,117 @@
-<div class="container">
-    <div class="row slider">
-        <div class="col-xl-12">
-            <img style="width: 1890px; height: 183px" class="item" src="<?=base_url()?>assets/web/img/pic1.jpg" alt="pic">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="slider">
+                <img style="width: 1890px; height: 183px" class="item" src="<?=base_url()?>assets/web/img/pic1.jpg" alt="pic">
+            </div>
         </div>
     </div>
     <div class="row content">
-        <div class="col-xl-6 col_left">
-            <div class="row title_time">
-                <h3><b>THÁNG 10 - 2017</b></h3>
+        <div class="col-xs-12 col-md-8">
+            <div class="bg-white">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="title_t">
+                            <h3><b>THÁNG 10 - 2017</b></h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="style_table">
+                            <table class="table">
+                                <tbody>
+                                <tr>
+                                    <th><h1>02</h1></th>
+                                    <td>
+                                        <span>
+                                            Staff’s Name:<br>
+
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at dapibus lacus. Integer interdum erat in massa eleifend posuere. Etiam luctus ipsum nisl, vitae malesuada libero ultricies vitae. Maecenas aliquam iaculis enim sit amet cursus. Praesent facilisis quam vel ipsum varius, nec facilisis nibh varius. Etiam molestie, augue et elementum scelerisque, neque quam accumsan urna, quis condimentum eros urna quis magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus mauris mauris, pulvinar eget neque sit amet, posuere rhoncus ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec dignissim nisi ut nisl accumsan gravida.
+                                            <br>Deadline: 18/10/2017<br>
+                                            <br>Staff’s Name:<br>
+
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at dapibus lacus. Integer interdum erat in massa eleifend posuere. Etiam luctus ipsum nisl, vitae malesuada libero ultricies vitae. Maecenas aliquam iaculis enim sit amet cursus. Praesent facilisis quam vel ipsum varius, nec facilisis nibh varius. Etiam molestie, augue et elementum scelerisque, neque quam accumsan urna, quis condimentum eros urna quis magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus mauris mauris, pulvinar eget neque sit amet, posuere rhoncus ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec dignissim nisi ut nisl accumsan gravida.
+                                            <br>Deadline: <span style="color: red">18/10/2017</span>
+                                        </span>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td><h1>01</h1></td>
+                                                    <td>
+                                        <span>
+                                            Staff’s Name:<br>
+
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at dapibus lacus. Integer interdum erat in massa eleifend posuere. Etiam luctus ipsum nisl, vitae malesuada libero ultricies vitae. Maecenas aliquam iaculis enim sit amet cursus. Praesent facilisis quam vel ipsum varius, nec facilisis nibh varius. Etiam molestie, augue et elementum scelerisque, neque quam accumsan urna, quis condimentum eros urna quis magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus mauris mauris, pulvinar eget neque sit amet, posuere rhoncus ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec dignissim nisi ut nisl accumsan gravida.
+                                            <br>Deadline: 18/10/2017<br>
+                                            Staff’s Name:<br>
+
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at dapibus lacus. Integer interdum erat in massa eleifend posuere. Etiam luctus ipsum nisl, vitae malesuada libero ultricies vitae. Maecenas aliquam iaculis enim sit amet cursus. Praesent facilisis quam vel ipsum varius, nec facilisis nibh varius. Etiam molestie, augue et elementum scelerisque, neque quam accumsan urna, quis condimentum eros urna quis magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus mauris mauris, pulvinar eget neque sit amet, posuere rhoncus ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec dignissim nisi ut nisl accumsan gravida.
+                                            <br>Deadline: 18/10/2017<br>
+                                            Staff’s Name:<br>
+
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at dapibus lacus. Integer interdum erat in massa eleifend posuere. Etiam luctus ipsum nisl, vitae malesuada libero ultricies vitae. Maecenas aliquam iaculis enim sit amet cursus. Praesent facilisis quam vel ipsum varius, nec facilisis nibh varius. Etiam molestie, augue et elementum scelerisque, neque quam accumsan urna, quis condimentum eros urna quis magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus mauris mauris, pulvinar eget neque sit amet, posuere rhoncus ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec dignissim nisi ut nisl accumsan gravida.
+                                            <br>Deadline: 18/10/2017<br>
+                                             Staff’s Name:<br>
+
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at dapibus lacus. Integer interdum erat in massa eleifend posuere. Etiam luctus ipsum nisl, vitae malesuada libero ultricies vitae. Maecenas aliquam iaculis enim sit amet cursus. Praesent facilisis quam vel ipsum varius, nec facilisis nibh varius. Etiam molestie, augue et elementum scelerisque, neque quam accumsan urna, quis condimentum eros urna quis magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus mauris mauris, pulvinar eget neque sit amet, posuere rhoncus ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec dignissim nisi ut nisl accumsan gravida.
+                                            <br>Deadline: 18/10/2017<br>
+                                             Staff’s Name:<br>
+
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at dapibus lacus. Integer interdum erat in massa eleifend posuere. Etiam luctus ipsum nisl, vitae malesuada libero ultricies vitae. Maecenas aliquam iaculis enim sit amet cursus. Praesent facilisis quam vel ipsum varius, nec facilisis nibh varius. Etiam molestie, augue et elementum scelerisque, neque quam accumsan urna, quis condimentum eros urna quis magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus mauris mauris, pulvinar eget neque sit amet, posuere rhoncus ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec dignissim nisi ut nisl accumsan gravida.
+                                            <br>Deadline: 18/10/2017<br>
+
+                                        </span>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <table class="table style_table">
-                <tr>
-                    <th><h1>02</h1></th>
-                    <th>
-                        <span>
-                            <b>Staff’s Name:</b><br>
-
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at dapibus lacus. Integer interdum erat in massa eleifend posuere. Etiam luctus ipsum nisl, vitae malesuada libero ultricies vitae. Maecenas aliquam iaculis enim sit amet cursus. Praesent facilisis quam vel ipsum varius, nec facilisis nibh varius. Etiam molestie, augue et elementum scelerisque, neque quam accumsan urna, quis condimentum eros urna quis magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus mauris mauris, pulvinar eget neque sit amet, posuere rhoncus ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec dignissim nisi ut nisl accumsan gravida.
-                            <br>Deadline: 18/10/2017<br>
-                            <br><b>Staff’s Name:</b><br>
-
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at dapibus lacus. Integer interdum erat in massa eleifend posuere. Etiam luctus ipsum nisl, vitae malesuada libero ultricies vitae. Maecenas aliquam iaculis enim sit amet cursus. Praesent facilisis quam vel ipsum varius, nec facilisis nibh varius. Etiam molestie, augue et elementum scelerisque, neque quam accumsan urna, quis condimentum eros urna quis magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus mauris mauris, pulvinar eget neque sit amet, posuere rhoncus ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec dignissim nisi ut nisl accumsan gravida.
-                            <br>Deadline: <span style="color: red">18/10/2017</span>
-                        </span>
-                    </th>
-                </tr>
-                <tr>
-                    <td><h1>01</h1></td>
-                    <td>
-                        <span>
-                            Staff’s Name:<br>
-
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at dapibus lacus. Integer interdum erat in massa eleifend posuere. Etiam luctus ipsum nisl, vitae malesuada libero ultricies vitae. Maecenas aliquam iaculis enim sit amet cursus. Praesent facilisis quam vel ipsum varius, nec facilisis nibh varius. Etiam molestie, augue et elementum scelerisque, neque quam accumsan urna, quis condimentum eros urna quis magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus mauris mauris, pulvinar eget neque sit amet, posuere rhoncus ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec dignissim nisi ut nisl accumsan gravida.
-                            <br>Deadline: 18/10/2017
-                        </span>
-                    </td>
-                </tr>
-            </table>
         </div>
-        <div class="col-xl-6 col_right">
-            <div class="row">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/RZ4NMweaUcQ" frameborder="0" allowfullscreen></iframe>
+
+        <div class="col-md-4 col_right">
+            <div class="video">
+                <iframe width="100%" height="395" src="https://www.youtube.com/embed/RZ4NMweaUcQ" frameborder="0" allowfullscreen></iframe>
+            </div>
+            <div class="bg-white noti">
+                <div class="row">
+                    <div class="col-xs-12 col-lg-12">
+                            <div class="noti_titile">
+                                <h3>THÔNG BÁO MỚI</h3>
+                            </div>
+                    </div>
+                </div>
+                <div class="line_hr">
+                    &nbsp;
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 col-lg-12">
+                        <div class="noti_text">
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at dapibus lacus <br>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at dapibus lacus. Integer interdum erat in massa eleifend posuere. Etiam luctus ipsum nisl, vitae malesuada libero ultricies vitae. Maecenas aliquam iaculis enim sit amet cursus.
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12 col-md-12 col-lg-12">
+            <div class="bg-white footer_body">
+                <div class="marquee"><h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at dapibus lacus.</h3></div>
             </div>
         </div>
     </div>
--- application/views/layout_web/include/footer.php
+++ application/views/layout_web/include/footer.php
@@ -3,5 +3,6 @@
 		<!-- Bootstrap JavaScript -->
 		<script src="<?=base_url()?>assets/web/js/bootstrap.min.js"></script>
         <script src="<?=base_url()?>assets/web/js/script.js"></script>
+
 	</body>
 </html>
